80b4494876694119aebfa9ad06ab45916e320489
YuyaNakamura0139 <mtk371@icloud.com>
2021-10-14 17:55:36 +0900

    add-cnn

---
+++ vgg16/cnn.py
@@ -0,0 +1,19 @@
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing import image
+from PIL import Image
+from project.settings import BASE_DIR
+import numpy as np
+import os
+
+
+model_path = os.path.join(BASE_DIR, 'works/cnn_model.h5')
+model = load_model(model_path)
+
+
+def classification(Img):
+    img_data = Img.resize((224, 224))
+    img_data = np.asarray(img_data)
+    img_data = img_data / 255.0
+    prd = model.predict(np.array([img_data]))
+
+    return prd[0][0]
